98f04aedc45aa61d4d59f7fec62f542f015140e1
hieucan99 <hieucan99@gmail.com>
2024-05-27 20:52:22 +0700

    add

---
+++ Test Suites/Run All API.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run All API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6e51ca25-8176-4db4-b4b3-1b37cf6787fc</testSuiteGuid>
+</TestSuiteEntity>
